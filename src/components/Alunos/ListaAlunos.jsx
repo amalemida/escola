5fca2e51f7c07a92c41ee0a22b32ce87bab82694
amalemida <amalemida@gmail.com>
2023-09-19 21:00:40 -0300

    Aula 19/09/2023

--- src/components/Alunos/ListaAlunos.jsx
+++ src/components/Alunos/ListaAlunos.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import styles from "@/styles/Alunos.module.css";
+
 export default function ListaAlunos() {
-  return <div>Lista Alunos</div>;
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5241/api/Aluno")
+      .then((resp) => resp.json())
+      .then((alunos) => setLista(alunos));
+
+    console.log(lista);
+  }, []);
+
+  return (
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th className={styles.th}>RA</th>
+            <th className={styles.th}>Nome</th>
+            <th className={styles.th}>Curso</th>
+          </tr>
+        </thead>
+        {lista.map((aluno) => {
+          return (
+            <tbody>
+              <tr>
+                <td className={styles.td}>{aluno.ra}</td>
+                <td className={styles.td}>{aluno.nome}</td>
+                <td className={styles.td}>{aluno.codCurso}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+  );
 }
